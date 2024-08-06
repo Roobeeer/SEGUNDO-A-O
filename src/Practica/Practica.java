1f8cd4229c01519859243c0f3fabf233f6743694
Roobeeer <furybtw1@gmail.com>
2024-08-06 15:23:16 +0200

    codigos actualizados

--- src/Practica/Practica.java
+++ src/Practica/Practica.java
@@ -1,4 +1,32 @@
 package Practica;
 
 public class Practica {
+    private int numero;
+    private NodoInt siguiente;
+    private NodoInt dato;
+
+
+}
+
+
+class NodoInt{
+    private int dato;
+    private NodoInt siguiente;
+
+    public void NodoInt(int dato, NodoInt siguiente){
+        this.dato = dato;
+        this.siguiente = siguiente;
+    }
+
+    public void NodoInt(int dato){
+        this.siguiente = null;
+        this.dato = dato;
+
+    }
+
+    public void NodoInt(NodoInt siguiente){
+        this.siguiente = siguiente;
+        this.dato = 0;
+
+    }
 }
